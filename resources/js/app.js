512e42662bcde8594ac25b90630aadc41a313e95
YouBou <33052528+YouBou@users.noreply.github.com>
2022-04-16 21:25:29 +0900

    [feat] new component

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,3 +1,5 @@
+import HeaderComponent from "./components/HeaderComponent";
+
 /**
  * First we will load all of this project's JavaScript dependencies which
  * includes Vue and other libraries. It is a great starting point when
@@ -19,7 +21,8 @@ window.Vue = require('vue').default;
 // const files = require.context('./', true, /\.vue$/i)
 // files.keys().map(key => Vue.component(key.split('/').pop().split('.')[0], files(key).default))
 
-Vue.component('example-component', require('./components/ExampleComponent.vue').default);
+// Vue.component('example-component', require('./components/ExampleComponent.vue').default);
+Vue.conponent('header-component', HeaderComponent);
 
 /**
  * Next, we will create a fresh Vue application instance and attach it to
@@ -29,4 +32,7 @@ Vue.component('example-component', require('./components/ExampleComponent.vue').
 
 const app = new Vue({
     el: '#app',
+    components: {
+        HeaderComponent
+    }
 });
