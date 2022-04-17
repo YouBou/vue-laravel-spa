62e74e2ccbe7c0c52d8ac89c59fc53913aec2ab5
YouBou <33052528+YouBou@users.noreply.github.com>
2022-04-17 23:17:32 +0900

    [feat] TaskListComponent

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,8 +1,24 @@
-import HeaderComponent from "./components/HeaderComponent";
 import { createApp } from 'vue';
+import VueRouter from 'vue-router';
+import HeaderComponent from "./components/HeaderComponent";
+import TaskListComponent from "./components/TaskListComponent";
 
 require('./bootstrap');
+
+VueRouter.use(VueRouter);
+
+const router = new VueRouter({
+    mode: 'history',
+    routes: [
+        {
+            path: '/tasks',
+            name: 'task.List',
+            component: TaskListComponent
+        },
+    ]
+});
 
 const app = createApp({})
 app.component('header-component', HeaderComponent);
 app.mount('#app')
+
