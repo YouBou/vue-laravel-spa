8c53c54535fa8b5ea0d9cbb70558d9ecf21b581a
YouBou <33052528+YouBou@users.noreply.github.com>
2022-04-20 23:40:14 +0900

    feat(component): add task show component

--- resources/js/routes.js
+++ resources/js/routes.js
@@ -1,11 +1,18 @@
 import {createRouter, createWebHistory} from 'vue-router';
 import TaskListComponent from "./components/TaskListComponent";
+import TaskShowComponent from "./components/TaskShowComponent";
 
 const routes = [
     {
         path: '/tasks',
         component: TaskListComponent,
         name: 'task.list',
+    },
+    {
+        path: '/tasks/:taskId',
+        component: TaskShowComponent,
+        name: 'task.show',
+        props: true
     }
 ];
 
