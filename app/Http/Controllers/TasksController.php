6704664e18ed8465ba0b2d8734d87756760c9fa7
YouBou <33052528+YouBou@users.noreply.github.com>
2022-04-25 09:35:58 +0900

    fix(controller): Rename tasks controller

--- app/Http/Controllers/TasksController.php
+++ app/Http/Controllers/TasksController.php
@@ -3,9 +3,14 @@
 namespace App\Http\Controllers;
 
 use App\Models\Task;
+use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
 
-class TaskController extends Controller
+/**
+ * Class TasksController
+ * @package App\Http\Controllers
+ */
+class TasksController extends Controller
 {
     public function index()
     {
--- routes/api.php
+++ routes/api.php
@@ -2,7 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\TaskController;
+use App\Http\Controllers\TasksController;
 
 /*
 |--------------------------------------------------------------------------
@@ -19,4 +19,4 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::get('/tasks', [TaskController::class, 'index']);
+Route::get('/tasks', [TasksController::class, 'index']);
